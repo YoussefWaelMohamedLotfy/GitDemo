1359e3e051202345b6b04a5084dcfe50a7556ddd
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-02-08 15:40:44 +0200

    Implement Add in main using Calculator

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿int number = 6;
+﻿using GitDemo;
+
+int number = 6;
 
 Console.WriteLine(IsOdd(number));
 
 
 Console.WriteLine(IsEven(number));
+
+Calculator calc = new Calculator();
+
+Console.WriteLine($"The sum of 5 and 4 is {calc.Sum(4, 5)}");
 
 bool IsEven(int num) => num % 2 == 0;
 
