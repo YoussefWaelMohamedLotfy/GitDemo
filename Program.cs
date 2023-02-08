6bb4b76831870a246eceb8c0c5c4588dc9319b50
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-02-08 15:20:29 +0200

    Refactor function to use expression bodies

--- Program.cs
+++ Program.cs
@@ -5,13 +5,6 @@ Console.WriteLine(IsOdd(number));
 
 Console.WriteLine(IsEven(number));
 
-bool IsEven(int num)
-{
-    return num % 2 == 0;
-}
+bool IsEven(int num) => num % 2 == 0;
 
-
-bool IsOdd(int num)
-{
-    return num % 2 != 0;
-}
+bool IsOdd(int num) => num % 2 != 0;
